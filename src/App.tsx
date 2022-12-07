e2e01581f6bb6c174be2659a5f8eaf62442b0218
zae <isaiah.iu@hotmail.com>
2022-12-07 10:48:21 -0500

    Import components into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import AngryButton from "./components/AngryButton";
+import CounterButton from "./components/CounterButton";
+import LightSwitchButton from "./components/LightSwitchButton";
+import TextRepeaterButton from "./components/TextRepeaterButton";
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<h1>Fancy Buttons!</h1>
+			<section>
+				<AngryButton />
+				<CounterButton />
+				<LightSwitchButton />
+				<TextRepeaterButton />
+			</section>
+		</div>
+	);
 }
 
 export default App;
